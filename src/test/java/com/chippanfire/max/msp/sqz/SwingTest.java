18730cfbd6c5fece5a94572339b98d7b22725794
Will <crossland.w.a@gmail.com>
2015-04-16 22:43:35 +0100

    Update SwingTest to reimplement tests for swinging via interfacing with MspStepperImpl

--- src/test/java/com/chippanfire/max/msp/sqz/SwingTest.java
+++ src/test/java/com/chippanfire/max/msp/sqz/SwingTest.java
@@ -17,7 +17,7 @@ public class SwingTest {
 
     @BeforeMethod
     public void setUp() throws Exception {
-        swing = swing.unswung(4);
+        swing = Swing.unswung(4);
         countingStubMaxComms.reset();
         stepper = new MspStepperImpl(countingStubMaxComms, 32);
     }
@@ -50,7 +50,6 @@ public class SwingTest {
             0f, 0.1f, 0.2f, 0.3f, 0.4f, 0.5f, 0.6f, 0.7f, 0.8f, 0.9f,
             0f, 0.1f
         );
-        assertEquals(countingStubMaxComms.messageCount(), 5);
         assertEquals(
             countingStubMaxComms.values(),
             new LinkedHashMap<Integer, Integer>(){{
@@ -59,6 +58,67 @@ public class SwingTest {
                 put(21, 2);
                 put(31, 3);
                 put(41, 4);
+            }}
+        );
+    }
+
+    @Test
+    public void everyOtherStepIsEarlyWith2StepSwingPattern() throws Exception {
+        stepper.rampTimeInSamples(10);
+        stepper.updateSwing(Arrays.asList(0f, 0.75f, 1f));
+
+        process(
+            0f, 0.1f, 0.2f, 0.3f, 0.4f, 0.5f, 0.6f, 0.7f, 0.8f, 0.9f,
+            0f, 0.1f, 0.2f, 0.3f, 0.4f, 0.5f, 0.6f, 0.7f, 0.8f, 0.9f,
+            0f, 0.1f, 0.2f, 0.3f, 0.4f, 0.5f, 0.6f, 0.7f, 0.8f, 0.9f,
+            0f, 0.1f, 0.2f, 0.3f, 0.4f, 0.5f, 0.6f, 0.7f, 0.8f, 0.9f,
+            0f, 0.1f, 0.2f, 0.3f, 0.4f, 0.5f, 0.6f, 0.7f, 0.8f, 0.9f,
+            0f, 0.1f, 0.2f, 0.3f, 0.4f, 0.5f, 0.6f, 0.7f, 0.8f, 0.9f,
+            0f, 0.1f, 0.2f, 0.3f, 0.4f, 0.5f, 0.6f, 0.7f, 0.8f, 0.9f,
+            0f, 0.1f, 0.2f, 0.3f, 0.4f, 0.5f, 0.6f, 0.7f, 0.8f, 0.9f
+        );
+        assertEquals(
+            countingStubMaxComms.values(),
+            new LinkedHashMap<Integer, Integer>(){{
+                put(1, 0);
+                put(8, 1);
+                put(21, 2);
+                put(28, 3);
+                put(41, 4);
+                put(48, 5);
+                put(61, 6);
+                put(68, 7);
+            }}
+        );
+    }
+
+    @Test
+    public void everyThirdStepIsLateWith4StepSwingPattern() throws Exception {
+        stepper.rampTimeInSamples(10);
+        stepper.updateSwing(Arrays.asList(0f, 0.25f, 0.3f, 0.75f, 1f));
+
+        process(
+            0f, 0.1f, 0.2f, 0.3f, 0.4f, 0.5f, 0.6f, 0.7f, 0.8f, 0.9f,
+            0f, 0.1f, 0.2f, 0.3f, 0.4f, 0.5f, 0.6f, 0.7f, 0.8f, 0.9f,
+            0f, 0.1f, 0.2f, 0.3f, 0.4f, 0.5f, 0.6f, 0.7f, 0.8f, 0.9f,
+            0f, 0.1f, 0.2f, 0.3f, 0.4f, 0.5f, 0.6f, 0.7f, 0.8f, 0.9f,
+            0f, 0.1f, 0.2f, 0.3f, 0.4f, 0.5f, 0.6f, 0.7f, 0.8f, 0.9f,
+            0f, 0.1f, 0.2f, 0.3f, 0.4f, 0.5f, 0.6f, 0.7f, 0.8f, 0.9f,
+            0f, 0.1f, 0.2f, 0.3f, 0.4f, 0.5f, 0.6f, 0.7f, 0.8f, 0.9f,
+            0f, 0.1f, 0.2f, 0.3f, 0.4f, 0.5f, 0.6f, 0.7f, 0.8f, 0.9f
+        );
+
+        assertEquals(
+            countingStubMaxComms.values(),
+            new LinkedHashMap<Integer, Integer>(){{
+                put(1, 0);
+                put(11, 1);
+                put(26, 2);
+                put(31, 3);
+                put(41, 4);
+                put(51, 5);
+                put(66, 6);
+                put(71, 7);
             }}
         );
     }
