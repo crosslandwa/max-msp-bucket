eaede458928b56e10f83ab8502ee74e3ff0881cf
Will <crossland.w.a@gmail.com>
2015-04-16 22:44:28 +0100

    Remove test cases from SwingTest that interacted directly with Swing class

--- src/test/java/com/chippanfire/max/msp/sqz/SwingTest.java
+++ src/test/java/com/chippanfire/max/msp/sqz/SwingTest.java
@@ -11,34 +11,12 @@ import static org.testng.AssertJUnit.assertEquals;
 
 public class SwingTest {
     private MspStepperImpl stepper;
-    private Swing swing;
-
     private SampleCountingStubMaxComms countingStubMaxComms = new SampleCountingStubMaxComms();
 
     @BeforeMethod
     public void setUp() throws Exception {
-        swing = Swing.unswung(4);
         countingStubMaxComms.reset();
         stepper = new MspStepperImpl(countingStubMaxComms, 32);
-    }
-
-    @Test
-    public void noInterpolationWhenNoSwing() throws Exception {
-        assertEquals(swing.process(0f), 0f);
-        assertEquals(swing.process(0.5f), 0.5f);
-        assertEquals(swing.process(1f), 1f);
-    }
-
-    @Test
-    public void interpolates() throws Exception {
-        swing = swing.withSwingValues(Arrays.asList(0f, 0.25f, 0.3f, 0.75f, 1f));
-
-        assertEquals(swing.process(0f), 0f);
-        assertEquals(swing.process(0.125f), 0.125f);
-        assertEquals(swing.process(0.25f), 0.25f);
-        assertEquals(swing.process(0.375f), 0.275f);
-        assertEquals(swing.process(0.5f), 0.3f);
-        assertEquals(swing.process(0.75f), 0.75f);
     }
 
     @Test
