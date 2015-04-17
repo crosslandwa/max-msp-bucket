648f7f0a2b107a84e03df0de2258add7ac83016e
crosslandwa <crossland.w.a@gmail.com>
2015-04-17 13:25:29 +0100

    Update SteppingTest to make comparison against stubMaxComms.values() instead of latest()

--- src/test/java/com/chippanfire/max/msp/sqz/SteppingTest.java
+++ src/test/java/com/chippanfire/max/msp/sqz/SteppingTest.java
@@ -5,7 +5,7 @@ import org.testng.annotations.Test;
 
 import java.util.LinkedHashMap;
 
-import static org.testng.Assert.assertEquals;
+import static junit.framework.Assert.assertEquals;
 
 public class SteppingTest extends MspStepperImplBaseTest {
 
@@ -13,14 +13,14 @@ public class SteppingTest extends MspStepperImplBaseTest {
     public void startsCleanly() throws Exception {
         rampStopped();
         fullRamp();
-        process(new float[] {0f, 0.01f});
+        process(new float[]{0f, 0.01f});
         rampStopped();
         rampStopped();
         rampStopped();
         rampStopped();
         rampStopped();
         fullRamp();
-        process(new float[] {0f, 0.01f});
+        process(new float[]{0f, 0.01f});
 
         AssertJUnit.assertEquals(
             new LinkedHashMap<Integer, Integer>() {{
@@ -35,67 +35,57 @@ public class SteppingTest extends MspStepperImplBaseTest {
 
     @Test
     public void outputsEachStep() throws Exception {
-        rampFirstHalf();
+        fullRamp();
+        fullRamp();
+        fullRamp();
 
-        assertEquals(stubMaxComms.latest(), 0);
-        assertEquals(stubMaxComms.messageCount(), 1);
-
-        rampSecondHalf().rampFirstHalf();
-
-        assertEquals(stubMaxComms.latest(), 1);
-        assertEquals(stubMaxComms.messageCount(), 2);
-
-        rampSecondHalf().rampFirstHalf();
-
-        assertEquals(stubMaxComms.latest(), 2);
-        assertEquals(stubMaxComms.messageCount(), 3);
+        assertEquals(
+            new LinkedHashMap<Integer, Integer>(){{
+                put(1, 0);
+                put(101, 1);
+                put(201, 2);
+            }},
+            stubMaxComms.values()
+        );
     }
 
     @Test
     public void rollsOverAtMax() throws Exception {
         stepper.numberOfSteps(3);
-        rampFirstHalf();
+        fullRamp();
+        fullRamp();
+        fullRamp();
+        fullRamp();
 
-        assertEquals(stubMaxComms.latest(), 0);
-        assertEquals(stubMaxComms.messageCount(), 1);
-
-        rampSecondHalf().rampFirstHalf();
-
-        assertEquals(stubMaxComms.latest(), 1);
-        assertEquals(stubMaxComms.messageCount(), 2);
-
-        rampSecondHalf().rampFirstHalf();
-
-        assertEquals(stubMaxComms.latest(), 2);
-        assertEquals(stubMaxComms.messageCount(), 3);
-
-        rampSecondHalf().rampFirstHalf();
-
-        assertEquals(stubMaxComms.latest(), 0);
-        assertEquals(stubMaxComms.messageCount(), 4);
+        assertEquals(
+            new LinkedHashMap<Integer, Integer>(){{
+                put(1, 0);
+                put(101, 1);
+                put(201, 2);
+                put(301, 0);
+            }},
+            stubMaxComms.values()
+        );
     }
 
     @Test
     public void restartsAtZeroAfterStopping() throws Exception {
-        rampFirstHalf();
+        fullRamp();
+        fullRamp();
+        process(new float[] {0f, 0.01f});
 
-        assertEquals(stubMaxComms.latest(), 0);
-        assertEquals(stubMaxComms.messageCount(), 1);
+        rampStopped();
+        fullRamp();
 
-        rampSecondHalf().rampFirstHalf();
-
-        assertEquals(stubMaxComms.latest(), 1);
-        assertEquals(stubMaxComms.messageCount(), 2);
-
-        rampSecondHalf().rampFirstHalf();
-
-        assertEquals(stubMaxComms.latest(), 2);
-        assertEquals(stubMaxComms.messageCount(), 3);
-
-        rampStopped().rampFirstHalf();
-
-        assertEquals(stubMaxComms.latest(), 0);
-        assertEquals(stubMaxComms.messageCount(), 4);
+        assertEquals(
+            new LinkedHashMap<Integer, Integer>(){{
+                put(1, 0);
+                put(101, 1);
+                put(201, 2);
+                put(213, 0);
+            }},
+            stubMaxComms.values()
+        );
     }
 
 }
