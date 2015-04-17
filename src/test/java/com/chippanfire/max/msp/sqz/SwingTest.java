c32c59a0237d3a64c15d1e7086c3791165da3abf
crosslandwa <crossland.w.a@gmail.com>
2015-04-17 09:31:45 +0100

    Correct order of expected/actual in assertion. Move configuration of rampTimeInSamples into setup

--- src/test/java/com/chippanfire/max/msp/sqz/SwingTest.java
+++ src/test/java/com/chippanfire/max/msp/sqz/SwingTest.java
@@ -17,6 +17,7 @@ public class SwingTest {
     public void setUp() throws Exception {
         countingStubMaxComms.reset();
         stepper = new MspStepperImpl(countingStubMaxComms, 32);
+        stepper.rampTimeInSamples(10);
     }
 
     @Test
@@ -29,20 +30,19 @@ public class SwingTest {
             0f, 0.1f
         );
         assertEquals(
-            countingStubMaxComms.values(),
             new LinkedHashMap<Integer, Integer>(){{
                 put(1, 0);
                 put(11, 1);
                 put(21, 2);
                 put(31, 3);
                 put(41, 4);
-            }}
+            }},
+            countingStubMaxComms.values()
         );
     }
 
     @Test
     public void everyOtherStepIsEarlyWith2StepSwingPattern() throws Exception {
-        stepper.rampTimeInSamples(10);
         stepper.updateSwing(Arrays.asList(0f, 0.75f, 1f));
 
         process(
@@ -56,7 +56,6 @@ public class SwingTest {
             0f, 0.1f, 0.2f, 0.3f, 0.4f, 0.5f, 0.6f, 0.7f, 0.8f, 0.9f
         );
         assertEquals(
-            countingStubMaxComms.values(),
             new LinkedHashMap<Integer, Integer>(){{
                 put(1, 0);
                 put(8, 1);
@@ -66,13 +65,13 @@ public class SwingTest {
                 put(48, 5);
                 put(61, 6);
                 put(68, 7);
-            }}
+            }},
+            countingStubMaxComms.values()
         );
     }
 
     @Test
     public void everyThirdStepIsLateWith4StepSwingPattern() throws Exception {
-        stepper.rampTimeInSamples(10);
         stepper.updateSwing(Arrays.asList(0f, 0.25f, 0.3f, 0.75f, 1f));
 
         process(
@@ -87,7 +86,6 @@ public class SwingTest {
         );
 
         assertEquals(
-            countingStubMaxComms.values(),
             new LinkedHashMap<Integer, Integer>(){{
                 put(1, 0);
                 put(11, 1);
@@ -97,7 +95,8 @@ public class SwingTest {
                 put(51, 5);
                 put(66, 6);
                 put(71, 7);
-            }}
+            }},
+            countingStubMaxComms.values()
         );
     }
 
